da711cf4d16dafb9e9f6268b69dfc045e4db50ba
ltpdev <1776494277@qq.com>
2018-06-21 11:38:43 +0800

    完成捐款列表的搜索，以及查看详细

--- World/Admin/Controller/DonationController.class.php
+++ World/Admin/Controller/DonationController.class.php
@@ -4,8 +4,8 @@ use Think\Controller;
 class DonationController extends CommonController {
 
     public function donation_list(){
-
-        $count = M("u_donation")->count();
+        $join="u_user as u on u.user_id=donation_uid";
+        $count = M("u_donation")->where("donation_static=1")->count();
         //实例化分页类
         $Page = new \Think\Page($count,$this->pagenum);
         //  $Page = new \Think\Page($count,20);
@@ -15,10 +15,64 @@ class DonationController extends CommonController {
         $Page->setConfig('theme', '%HEADER% %FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
         //显示分页信息
         $show = $Page->show();// 分页显示输出 ->limit($Page->firstRow.','.$Page->listRows)
-        $list = M("u_donation")->limit($Page->firstRow.','.$Page->listRows)->order("donation_id DESC")->select();
+        //$field = "u_donation.*,";
+        $list = M("u_donation")->join($join)->where("donation_static=1")->limit($Page->firstRow.','.$Page->listRows)->order("donation_id DESC")->select();
         $this->assign('list',$list);
         $this->assign('page',$show);
         $this->display("donation_list");
     }
+
+    public function donation_list_sou(){
+        $join="u_user as u on u.user_id=donation_uid";
+        $where="donation_static=1";
+        $nickname = trim($_POST['nickname']);
+        if($nickname){
+            $where .= " and u.user_name like '%$nickname%'";
+            $this->assign('nickname',$nickname);
+        }
+        $email= trim($_POST['email']);
+        if($email){
+            $where .= " and u.user_mail like '%$email%'";
+            $this->assign('email',$email);
+        }
+        $stime = trim($_POST['stime']);
+	   if($stime){
+     		$where .= " and UNIX_TIMESTAMP(donation_paytime) >= UNIX_TIMESTAMP('$stime 00:00:00')";
+     		$this->assign('stime',$stime);
+     	}
+
+     	$etime = trim($_POST['etime']);
+     	if($etime){
+     		$where .= " and UNIX_TIMESTAMP(donation_paytime) <= UNIX_TIMESTAMP('$etime 23:59:59')";
+     		$this->assign('etime',$etime);
+     	}
+        $count = M("u_donation")->join($join)->where($where)->count();
+        //实例化分页类
+        $Page = new \Think\Page($count,$this->pagenum);
+        //  $Page = new \Think\Page($count,20);
+        //设置上一页与下一页
+        $Page->setConfig('prev', '上一页');
+        $Page->setConfig('next', '下一页');
+        $Page->setConfig('theme', '%HEADER% %FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
+        //显示分页信息
+        $show = $Page->show();// 分页显示输出 ->limit($Page->firstRow.','.$Page->listRows)
+        //$field = "u_donation.*,";
+        $list = M("u_donation")->join($join)->where($where)->limit($Page->firstRow.','.$Page->listRows)->order("donation_id DESC")->select();
+        $this->assign('list',$list);
+        $this->assign('page',$show);
+        $this->display("donation_list");
+    }
+
+
+    //查看详情
+    public function donation_detail(){
+        $id=$_GET["id"];
+        $join="u_user as u on u.user_id=donation_uid";
+        $donation=M("u_donation")->join($join)->where("donation_id={$id}")->find();
+        $this->assign("donation",$donation);
+        $this->display("donation_detail");
+    }
+
+
 
 }
--- World/Common/Lang/zh-cn.php
+++ World/Common/Lang/zh-cn.php
@@ -38,6 +38,7 @@ return array(
     'User_user_list_del' => '删除',
     'User_user_list_detail' => '查看',
     'User_user_list_code' => '邀请码',
+    'User_user_list_address' => '地址',
     'User_user_list_search' => '搜索',
     //user_list end
 
@@ -79,7 +80,13 @@ return array(
     /******************************************************************
      * Donation start
      */
-
+    //donation_list start
+    'Donation_donation_list_serial_number' => '序号',
+    'Donation_donation_list_donor' => '捐款人',
+    'Donation_donation_list_donation_amount' => '捐款金额(元)',
+    'Donation_donation_list_virtual_currency' => '奖励虚拟币',
+    'Donation_donation_list_donation_date' => '捐款日期',
+    //donation_list end
 
     /*
 	 * Donation end
