21bc0468a588cea607a44c3d39c036df8c6dae1a
wangxl <821754186@qq.com>
2018-06-25 17:10:14 +0800

    xl css/myPosts.css js/changeimg.js Rnterst/groupDetailsRelease.html

--- Public/Web/web/js/changeimg.js
+++ Public/Web/web/js/changeimg.js
@@ -1,3 +1,23 @@
+
+
+var values=[];
+var file_input = document.getElementById("file_input");
+file_input.addEventListener("change",function(){
+	    for(let key in file_input.files){
+	         	//只遍历对象自身的属性，而不包含继承于原型链上的属性。  
+	        if (file_input.files.hasOwnProperty(key) === true){
+	            values.unshift(file_input.files[key]);   
+	        }  
+	    }
+	    // console.log(values)
+	})
+
+
+
+
+
+
+
 var result = document.getElementById("result");
 			var input = document.getElementById("file_input");
 
@@ -42,12 +62,21 @@ var result = document.getElementById("result");
 							Cdiv.className = "covers"; //遮罩层的类名
 							ndiv.appendChild(Cdiv); //遮罩层拼接进去
                             result.prepend(ndiv); //将拼接进去最前面
-                            // *删除功能*/
-                        $(".covers").click(function() {
-                            var _this = $(this);
-							$("#file_input").val("");
-                            _this.parents(".img-div").remove();
-                        });
+							// *删除功能*/
+							// var num;
+							// $("#result").on('click',".img-div", function(event) {
+							// 	// event.stopPropagation()
+							// 	num = $(this).index();
+							// 	// console.log(num)
+							// });
+							$(".covers").click(function() {
+								event.stopPropagation()
+								var _this = $(this);
+								$("#file_input").val("");
+								var num = $("#result .img-div .covers").index(this)
+								values.splice(num,1)
+								_this.parents(".img-div").remove();
+							});
 
 						}
 					} else if(/video\/\w+/.test(file.type)) {
@@ -104,7 +133,7 @@ var result = document.getElementById("result");
 						//console.log([result])
 						$(".img-div").remove();
                         result.prepend(ndiv);
-                         // *删除功能*/
+						 // *删除功能*/
                         $(".covers").click(function() {
 							var _this = $(this);
 							$("#file_input").val("");
